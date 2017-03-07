f29330e9a2f8823e7e51c4358c9e54130890c643
Grayson Pike <graysonpike@gmail.com>
2017-03-06 21:22:52 -0600

    Crawler follows angle instead of x,y

--- entities/crawler.cpp
+++ entities/crawler.cpp
@@ -16,20 +16,16 @@ std::string Crawler::get_texture_name() {
 }
 
 void Crawler::update(float delta) {
-    if(player->get_x() > x) {
-        x += speed * delta;
-    } else if (player->get_x() < x) {
-        x -= speed * delta;
-    }
-    if(player->get_y() > y) {
-        y += speed * delta;
-    } else if (player->get_y() < y) {
-        y -= speed * delta;
+    // If approximate distance is less than moving distance
+    // just move to player coords
+    float x_dist = player->get_x() - x;
+    float y_dist = player->get_y() - y;
+    if(pow(speed * delta, 2) >= x_dist * x_dist + y_dist * y_dist) {
+        x = player->get_x();
+        y = player->get_y();
+    } else {
+        float angle = atan2(player->get_y() - y, player->get_x() - x);
+        x += delta * speed * cos(angle);
+        y += delta * speed * sin(angle);
     }
 }
-
-/*
-void Crawler::render(float delta) {
-    return;
-}
-*/
