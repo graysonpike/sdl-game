2cfd137d92fafb0ef7315a6cd789f6a1fae98e23
Grayson Pike <graysonpike@gmail.com>
2017-04-11 18:15:44 -0500

    Styling for missile.h, .cpp

--- entities/missile.cpp
+++ entities/missile.cpp
@@ -1,45 +1,16 @@
+#include <math.h>
 #include "missile.h"
 #include "player.h"
 #include "../debug.h"
-#include <math.h>
 
 #define LIFETIME (3.0f) // (in seconds)
+
+// STATIC MEMBERS
 
 const int Missile::w = 12;
 const int Missile::h = 20;
 
-const int Missile::get_id() {
-    return 1;
-}
-
-const bool Missile::collides() {
-    return false;
-}
-
-// Unused functions
-bool Missile::does_collide(int id) { return false; }
-void Missile::collide_entity(Entity *entity) {
-    switch(entity->get_id()) {
-        case 0:
-            if(((Player*)entity)->get_player_num() != player_num) {
-                alive = false;
-            }
-            break;
-        default:
-            break;
-    }
-}
-
-Missile::Missile(float x, float y, float speed, float angle, int player_num, int screen_w, int screen_h) : Entity(x, y) {
-    hitbox = new Hitbox(0, 0, w, h);
-    time_alive = 0.0f;
-    alive = true;
-    this->speed = speed;
-    this->angle = angle;
-    this->screen_w = screen_w;
-    this->screen_h = screen_h;
-    this->player_num = player_num;
-}
+// PRIVATE HELPER FUNCTIONS
 
 float Missile::get_center_x() {
     return x + w/2;
@@ -49,11 +20,30 @@ float Missile::get_center_y() {
     return y + h/2;
 }
 
+// PUBLIC FUNCTIONS
+
+Missile::Missile(float x, float y, float speed, float angle, int player_num,
+                 int screen_w, int screen_h) : Entity(x, y) {
+
+    hitbox = new Hitbox(0, 0, w, h);
+    time_alive = 0.0f;
+    alive = true;
+    this->speed = speed;
+    this->angle = angle;
+    this->screen_w = screen_w;
+    this->screen_h = screen_h;
+    this->player_num = player_num;
+
+}
+
 void Missile::update(float delta) {
+
     time_alive += delta;
+
     // Apply change in x and y directions
     x += delta * speed * cos(angle);
     y += delta * speed * sin(angle);
+
     // Check for going out of bounds
     if(this->get_center_x() < 0) {
         x = screen_w - w/2;
@@ -65,34 +55,67 @@ void Missile::update(float delta) {
     } else if (this->get_center_y() > screen_h) {
         y = 0 - h/2;
     }
+
     hitbox->update_pos(x, y, angle);
+
 }
 
-void Missile::render(SDL_Renderer *renderer, Resources *resources, float delta) {
+void Missile::render(SDL_Renderer *renderer, Resources *resources,
+                     float delta) {
 
+    // Render hitbox corners if enabled from debug.h
     #ifdef DEBUG_RENDER_HITBOX_CORNERS
     hitbox->render_corners(renderer);
     #endif
 
-    int texture_width, texture_height;
+    // Choose texture (changes missile color)
     SDL_Texture *texture;
     if(player_num == 1) {
         texture = resources->get_texture("missile1", 1);
     } else {
         texture = resources->get_texture("missile2", 1);
     }
+
+    int texture_width, texture_height;
     SDL_QueryTexture(texture, NULL, NULL, &texture_width, &texture_height);
+    
     SDL_Rect dst = {
         (int)x,
         (int)y,
         texture_width,
         texture_height
     };
-    SDL_RenderCopyEx(renderer, texture, NULL, &dst, angle / M_PI * 180 + 90, NULL, SDL_FLIP_NONE);
+
+    // Convert angle to degrees for SDL_RenderCopyEx()
+    float rect_angle = angle / M_PI * 180 + 90;
+
+    SDL_RenderCopyEx(renderer, texture, NULL, &dst, rect_angle,
+                     NULL, SDL_FLIP_NONE);
+
 }
 
 bool Missile::is_alive() {
     return time_alive < LIFETIME && alive;
+}
+
+const int Missile::get_id() {
+    return 1;
+}
+
+const bool Missile::collides() { return false; }
+
+bool Missile::does_collide(int id) { return false; }
+
+void Missile::collide_entity(Entity *entity) {
+    switch(entity->get_id()) {
+        case 0:
+            if(((Player*)entity)->get_player_num() != player_num) {
+                alive = false;
+            }
+            break;
+        default:
+            break;
+    }
 }
 
 int Missile::get_player_num() {
--- entities/missile.h
+++ entities/missile.h
@@ -5,11 +5,13 @@
 #include "../game/hitbox.h"
 #include <string>
 
+// Missile that travels in a straight line from where it was
+// fired at a given angle. Dies upon colliding with Player,
+// does not check for collisions. Dies after 3 seconds.
 class Missile: public Entity {
 
     static const int w, h;
-    // Speed in px/sec
-    float speed;
+    float speed; // Speed in px/sec
     float angle;
     int screen_w, screen_h;
     float time_alive;
@@ -20,15 +22,36 @@ class Missile: public Entity {
     float get_center_y();
 
 public:
-    Missile(float x, float y, float speed, float angle, int player_num, int screen_w, int screen_h);
+
+    // Missile entity at (x, y). Travels straight at (speed) px/s at
+    // (angle) radians. Belongs to player (player_num).
+    Missile(float x, float y, float speed, float angle, int player_num,
+            int screen_w, int screen_h);
+
+    // Moves the missile.
+    // If the missile moves out of screen bounds will wrap to the other side.
     void update(float delta);
+
     void render(SDL_Renderer *renderer, Resources *resources, float delta);
+
+    // Missile can die from colliding with a Player of a different player_num
+    // or living >3 seconds.
     bool is_alive();
+    
     const int get_id();
+
+    // Missiles don't check for collisions
     const bool collides();
+
+    // Unused
     bool does_collide(int id);
+
+    // If a missile collides with a Player with a different (player_num),
+    // it will die.
     void collide_entity(Entity *entity);
+
     int get_player_num();
+
 };
 
 #endif
